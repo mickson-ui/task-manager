df204fc2e956ec7b55dfe80e8bcfc37ebaba737b
Mickson Bonsu Bossman <mbonsu69@outlook.com>
2023-11-04 12:00:37 +0100

    task manager first commit

---
+++ app/Components/Sidebar/Sidebar.tsx
@@ -0,0 +1,15 @@
+"use client"
+import React from 'react'
+import styled from 'styled-components'
+
+function Sidebar() {
+  return (
+    <SidebarStyled>Sidebar</SidebarStyled>
+  )
+}
+
+const SidebarStyled = styled.nav`
+    
+`
+
+export default Sidebar
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Sidebar from './Components/Sidebar/Sidebar'
+import GlobalStyledProvider from './providers/GlobalStyledProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +18,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <GlobalStyledProvider>
+          <Sidebar />
+          {children}
+        </GlobalStyledProvider>
+        </body>
     </html>
   )
 }
